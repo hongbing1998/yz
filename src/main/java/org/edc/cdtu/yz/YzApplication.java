8db3fe4861a3a5a1dd6fb5345384cfbd9466c137
hongbing1998 <1227911097@qq.com>
2019-06-25 09:55:28 +0800

    整合大量配置。

--- src/main/java/org/edc/cdtu/yz/YzApplication.java
+++ src/main/java/org/edc/cdtu/yz/YzApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-//@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class}) //exclude:做连接数据库使用 无则报：Failed to determine a suitable driver class
 @SpringBootApplication
-@MapperScan("org.edc.cdtu.yz.mapper")
+@MapperScan(basePackages = "org.edc.cdtu.yz.mapper")
 public class YzApplication {
     public static void main(String[] args) {
         SpringApplication.run(YzApplication.class, args);
---
+++ src/test/java/org/edc/cdtu/yz/YzApplicationTests.java
@@ -0,0 +1,32 @@
+package org.edc.cdtu.yz;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class YzApplicationTests {
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void DataSourceTest() throws SQLException {
+        System.out.println("数据源：" + dataSource.getClass());
+        System.out.println("数据库连接：" + dataSource.getConnection());
+    }
+
+    @Test
+    public void MybatisTest() {
+        System.out.println("sqlSessionFactory = " + sqlSessionFactory);
+    }
+}
