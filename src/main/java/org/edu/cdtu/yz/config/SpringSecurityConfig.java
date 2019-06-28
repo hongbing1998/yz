13facb46dc6d02a565071d69574e0aeb00d617dd
hongbing1998 <1227911097@qq.com>
2019-06-28 11:38:42 +0800

    导入jstl依赖，开放Spring Security拦截...

---
+++ src/main/java/org/edu/cdtu/yz/config/SpringSecurityConfig.java
@@ -0,0 +1,14 @@
+package org.edu.cdtu.yz.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+    }
+}
