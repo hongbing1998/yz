3925b51752b63c3503c8bf29d72be7b7bf264d70
weiyuhang <2089853250@qq.com>
2019-07-01 22:18:49 +0800

    查询菜单

---
+++ src/main/java/org/edu/cdtu/yz/mapper/MenuMapper.java
@@ -0,0 +1,27 @@
+package org.edu.cdtu.yz.mapper;
+
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+public interface MenuMapper {
+    @Results(id = "roleResult", value = {
+            @Result(property = "parentId", column = "father_permission"),
+            @Result(property = "menuId", column = "id"),
+            @Result(property = "name", column = "permission_name"),
+            @Result(property = "url", column = "permission_url")
+
+    })
+    @Select("select DISTINCT p.father_permission,p.id,p.permission_name,p.permission_url" +
+            " from tb_permission as p,tb_role as r,tb_user as u,tb_role_user as ru," +
+            "tb_permission_role as pr where" +
+            "  u.username=#{username} and u.id=ru.user_id and ru.role_id=r.id and " +
+            " r.id=pr.roleId and pr.permissionId=p.id ")
+    public List<Map<String, Object>> getAllMenu(String username);
+
+
+}
+
---
+++ src/main/java/org/edu/cdtu/yz/service/impl/MenuserviceImpl.java
@@ -0,0 +1,43 @@
+package org.edu.cdtu.yz.service.impl;
+
+import org.edu.cdtu.yz.mapper.MenuMapper;
+import org.edu.cdtu.yz.service.IMenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class MenuserviceImpl implements IMenuService {
+    @Autowired
+    MenuMapper menuMapper;
+
+    public List<Map<String, Object>> getMenu(String username) {
+        System.out.println(username);
+        List<Map<String, Object>> allList = menuMapper.getAllMenu(username);
+        List<Map<String, Object>> parentList = new ArrayList<Map<String, Object>>();
+        List<Map<String, Object>> childList = new ArrayList<Map<String, Object>>();
+        for (Map<String, Object> menu : allList) {
+            if (menu.get("parentId").equals("0")) {
+                parentList.add(menu);
+            } else {
+                childList.add(menu);
+            }
+        }
+        for (Map<String, Object> pMenu : parentList) {
+            List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+            for (Map<String, Object> cMenu : childList) {
+                if (cMenu.get("parentId").equals(pMenu.get("menuId"))) {
+                    list.add(cMenu);
+                }
+            }
+            pMenu.put("childNode", list);
+
+        }
+        System.out.println("fdgddgd");
+        return parentList;
+
+    }
+}
