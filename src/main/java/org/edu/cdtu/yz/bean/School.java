3c6b67d5ee34dd4c30072eb882da9f31acbbc2e3
hongbing1998 <1227911097@qq.com>
2019-07-02 11:39:57 +0800

    .....

--- src/main/java/org/edu/cdtu/yz/bean/School.java
+++ src/main/java/org/edu/cdtu/yz/bean/School.java
@@ -3,52 +3,38 @@ package org.edu.cdtu.yz.bean;
 import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.activerecord.Model;
 import com.baomidou.mybatisplus.annotations.TableName;
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author wencheng
  * @since 2019-06-27
  */
+@Data
 @TableName("tb_school")
 public class School extends Model<School> {
-
     private static final long serialVersionUID = 1L;
-
     private String id;
     @TableField("school_name")
     private String schoolName;
-
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSchoolName() {
-        return schoolName;
-    }
-
-    public void setSchoolName(String schoolName) {
-        this.schoolName = schoolName;
-    }
+    @TableField("school_info")
+    private String schoolInfo;
+    @TableField("school_needhelp")
+    private Boolean schoolNeedHelp;
+    @TableField("school_operate_id")
+    private Boolean schoolOperateId;
+    @TableField("school_operate_name")
+    private Boolean schoolOperateName;
+    @TableField("school_operate_time")
+    private Boolean schoolOperateTime;
 
     @Override
     protected Serializable pkVal() {
         return this.id;
-    }
-
-    @Override
-    public String toString() {
-        return "School{" +
-        ", id=" + id +
-        ", schoolName=" + schoolName +
-        "}";
     }
 }
--- src/main/java/org/edu/cdtu/yz/controller/SchoolController.java
+++ src/main/java/org/edu/cdtu/yz/controller/SchoolController.java
@@ -11,68 +11,70 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/school")
 public class SchoolController {
     @Autowired
-    public ISchoolService schoolService;
+    private ISchoolService schoolService;
 
     /**
      * 保存、修改 【区分id即可】
-     * @param school  传递的实体
+     *
+     * @param school 传递的实体
      * @return Ajaxresult转换结果
      */
-    @RequestMapping(value="/save",method= RequestMethod.POST)
-    public AjaxResult save(@RequestBody School school){
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public AjaxResult save(@RequestBody School school) {
         try {
-            if(school.getId()!=null){
-                    schoolService.updateById(school);
-            }else{
-                    schoolService.insert(school);
+            if (school.getId() != null) {
+                schoolService.updateById(school);
+            } else {
+                schoolService.insert(school);
             }
             return AjaxResult.me();
         } catch (Exception e) {
             e.printStackTrace();
-            return AjaxResult.me().setMessage("保存对象失败！"+e.getMessage());
+            return AjaxResult.me().setMessage("保存对象失败！" + e.getMessage());
         }
     }
 
     //删除对象信息
-    @RequestMapping(value="/{id}",method=RequestMethod.DELETE)
-    public AjaxResult delete(@PathVariable("id") Long id){
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public AjaxResult delete(@PathVariable("id") Long id) {
         try {
             schoolService.deleteById(id);
             return AjaxResult.me();
         } catch (Exception e) {
             e.printStackTrace();
-            return AjaxResult.me().setMessage("删除对象失败！"+e.getMessage());
+            return AjaxResult.me().setMessage("删除对象失败！" + e.getMessage());
         }
     }
 
-    //获取用户
-    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
-    public School get(@PathVariable("id")Long id)
-    {
+    // 获取用户
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public School get(@PathVariable("id") Long id) {
         return schoolService.selectById(id);
     }
 
 
-    //查看所有的员工信息
-    @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public List<School> list(){
+    // 查看所有的学校信息
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public List<School> list() {
         return schoolService.selectList(null);
     }
 
 
     /**
-    * 分页查询数据：
-    * @param query 查询对象
-    * @return PageList 分页对象
-    */
-    @RequestMapping(value = "/json",method = RequestMethod.POST)
+     * 分页查询数据：
+     *
+     * @param query 查询对象
+     * @return PageList 分页对象
+     */
+    @RequestMapping(value = "/json", method = RequestMethod.POST)
     public PageList<School> json(@RequestBody PageQuery query) {
-        Page<School> page = new Page<School>(query.getPage(),query.getRows());
+        Page<School> page = new Page<>(query.getPage(), query.getRows());
         page = schoolService.selectPage(page);
-        return new PageList<School>(page.getPages(), page.getRecords());
+        return new PageList<>(page.getPages(), page.getRecords());
     }
 }
