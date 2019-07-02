9fac92ee3a75aa9c0120b23e76622dbb2730bfab
weiyuhang <2089853250@qq.com>
2019-07-02 13:14:54 +0800

    修改

--- src/main/java/org/edu/cdtu/yz/controller/PermissionRoleController.java
+++ src/main/java/org/edu/cdtu/yz/controller/PermissionRoleController.java
@@ -9,8 +9,6 @@ import org.edu.cdtu.yz.util.PageList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @CrossOrigin
 @RestController
 @RequestMapping("/permissionRole")
@@ -52,16 +50,16 @@ public class PermissionRoleController {
 
     //获取用户
     @RequestMapping(value = "/{id}",method = RequestMethod.GET)
-    public PermissionRole get(@PathVariable("id")Long id)
+    public AjaxResult get(@PathVariable("id") Long id)
     {
-        return permissionRoleService.selectById(id);
+        return AjaxResult.me().setResultObj(permissionRoleService.selectById(id));
     }
 
 
     //查看所有的员工信息
     @RequestMapping(value = "/list",method = RequestMethod.GET)
-    public List<PermissionRole> list(){
-        return permissionRoleService.selectList(null);
+    public AjaxResult list() {
+        return AjaxResult.me().setResultObj(permissionRoleService.selectList(null));
     }
 
 
@@ -71,9 +69,9 @@ public class PermissionRoleController {
     * @return PageList 分页对象
     */
     @RequestMapping(value = "/json",method = RequestMethod.POST)
-    public PageList<PermissionRole> json(@RequestBody PageQuery query) {
+    public AjaxResult json(@RequestBody PageQuery query) {
         Page<PermissionRole> page = new Page<PermissionRole>(query.getPage(),query.getRows());
         page = permissionRoleService.selectPage(page);
-        return new PageList<PermissionRole>(page.getPages(), page.getRecords());
+        return AjaxResult.me().setResultObj(new PageList<PermissionRole>(page.getPages(), page.getRecords()));
     }
 }
